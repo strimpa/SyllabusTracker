bc3fd8990641ddaa32295e8b27e5aa935ae73fc4
Gunnar Droege <strimpa@canyonville.dreamhost.com>
2019-07-16 02:56:32 -0700

    working filtering with multiple belts i.e. same-level exercise groups

--- views_classbased/syllabus.py
+++ views_classbased/syllabus.py
@@ -89,21 +89,25 @@ class SyllabusView(View):
         #find groups per order
         ratings_by_exercise = self.get_ratings(membership, is_summary, selected_memberships)
         groups = ExerciseGroup.objects.all()
-        root_group_names = ['Kyu', 'Waza']
+        group_names = ['Kyu', 'Waza']
         filter = None
         if 'filter' in kwargs and kwargs['filter']!=None:
             filter = kwargs['filter']
             filter = unquote(filter)
-            root_group_names = filter.split(',')
+            group_names = filter.split(',')
             
         #prefetch leaves
 #        start = time.time()
-        root_group_leaves = {}
-        for name in root_group_names:
+        group_leaves = {} # All end points to attach an exercise to
+        group_groups = {} # Grouping of exercise groups that are optional to have an exercise to attach to, i.e. the algorithm shouldn't stop until searching all groups of this group_group
+        for name in group_names:
             try:
                 group = ExerciseGroup.objects.select_related().get(name=name)
                 root_group = group.get_group_root()
-                root_group_leaves[name] = group.collect_leaves()
+                if root_group not in group_groups:
+                    group_groups[root_group] = []
+                group_groups[root_group].append(name)
+                group_leaves[name] = group.collect_leaves()
             except:
                 messages.info(request, "Exercise Group not found:"+name)
                 return redirect('/syllabus/')
@@ -128,16 +132,23 @@ class SyllabusView(View):
 #            print (str(ex))
             #create the tree of sub groups
             current_root = display_root
-            for name in root_group_names:
-                leaves = root_group_leaves[name]
-                for group in ex.groups.all():
-                    if group in leaves:
-#                        print("appending "+str(group))
-                        group_leaf = DisplayLeaf.find_or_create_leaf(group, current_root, current_root.depth+1)
-                        current_root = group_leaf
-                        append_exercise = name == root_group_names[-1]
-                        if append_exercise:
-                            group_leaf.exercises.append((ex, rating))  
+            all_groups_in_filter = True
+            for group_group_root in group_groups:
+                for name in group_groups[group_group_root]:
+                    leaves = group_leaves[name]
+                    for group in ex.groups.all():
+                        if group in leaves:
+    #                        print("appending "+str(group))
+                            group_leaf = DisplayLeaf.find_or_create_leaf(group, current_root, current_root.depth+1)
+                            current_root = group_leaf
+                            append_exercise = name == group_names[-1]
+                            if append_exercise:
+                                group_leaf.exercises.append((ex, rating))  
+                    
+                #Break to next exercise if the first group in the exercise has not been found in any root group defined in filter
+                if current_root == display_root:
+                    break
+
 #            ex3 = time.time()
 #            print("time 2:"+str(ex3 - ex2))
 
