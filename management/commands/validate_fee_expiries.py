9511fd92be964565718edb164ea3f524f110f97d
Gunnar Droege <mail@gunnardroege.de>
2019-07-31 01:46:07 +0100

    notification update

--- management/commands/validate_fee_expiries.py
+++ management/commands/validate_fee_expiries.py
@@ -1,3 +1,4 @@
+import datetime
 from django.core.management.base import BaseCommand, CommandError
 from SyllabusTrackerApp.models import Membership, FeeExpiry
 
@@ -9,5 +10,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for member in Membership.objects.all():
+            self.stdout.write(member.user.full_name())
             for fee_expiry in member.fees.all():
-                self.stdout.write(fee_expiry.fee_definition.name + " expires on "+str(fee_expiry.fee_expiry_date))
+                if fee_expiry.fee_expiry_date == None:
+                    self.stdout.write("\t- "+fee_expiry.fee_definition.name + " hasn't been set.")
+                else:
+                    difference = fee_expiry.fee_expiry_date - datetime.date.today()
+                    if difference.days < 0:
+                        self.stdout.write("\t- "+fee_expiry.fee_definition.name + " is overdue for "+str(abs(difference.days))+" days!")
+                        
+                    else:
+                        self.stdout.write("\t- "+fee_expiry.fee_definition.name + " expires on "+str(fee_expiry.fee_expiry_date))
